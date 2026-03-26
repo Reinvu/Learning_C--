3d24cbd8c1bb1550925623f280ce11df1c7d5796
Reinvu <shapavel2000@gmail.com>
2026-03-26 20:46:44 +0500

    first commit

---
+++ Array/1.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+
+    // №1
+
+    // int const n = 10, m = 5, a = 10, b = 20;
+    // int mass[n], mass1[m], mass2[m];
+
+
+    // for (int i = 0; i < n; i++){
+    //     cout<< "Введите числа первого массива: ";
+    //     cin>> mass[i]; 
+    //     cout<< endl;
+    // }
+
+
+    // for (int i = 0; i < m; i++){
+    // mass1[i] = mass[i];
+    // mass2[i] = mass[i+5];
+    // }
+
+    // for (int i = 0; i < m; i++){
+    //     cout << mass1[i] << "   " << mass2[i] << endl;
+    // }
+
+
+
+    // №2
+
+    // int const n = 10, a = 10, b = 20;
+    // int mass1[n], mass2[n], result[n];
+
+
+    // for (int i = 0; i < n; i++){
+    //     cout<< "Введите число первого массива: ";
+    //     cin>> mass1[i];
+    //     cout<< endl;
+
+    //     cout<< "Введите число второго массива массива: ";
+    //     cin>> mass2[i];
+    //     cout<< endl;
+
+    //     result[i] = mass1[i] + mass2[i];  
+    // }
+
+    // for(int i = 0; i < n; i++){
+    //     cout << mass1[i] << " + " << mass2[i];
+    //     cout <<" = "<< result[i]<< endl;
+    // }
+
+
+
+    // №3
+
+    // int mony[7];
+    // int dear_day = 0;
+    // int total_amount = 0;
+    // int average_amount;
+    // string week[7] = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+    // for(int i = 0; i < 7; i++){
+    //     cout<< "Введите сумму потраченную за "<< week[i] << ": ";
+    //     cin>> mony[i];
+    //     if(mony[i] > 100){
+    //         cout<< "В " << week[i] << " вы потратили более 100 долларов " << endl;
+    //         dear_day += 1;
+    //     }
+    //     total_amount += mony[i];
+    // }
+    // average_amount = total_amount / 7;
+
+    // cout<< "За неделю в среднем вы потратили " << average_amount << " долларов" << endl;
+    // cout<< "Всего за неделю вы потратили " << total_amount << " долларов" << endl;
+    // cout<< "Дней когда вы потратили более 100 долларов " << dear_day << " дня";
+
+
+
+    // №4
+
+    // double deposit;
+    // double rate[12];      // курс доллара к евро по месяцам
+    // double percent[12];   // процент начисления по месяцам
+    // int month;
+
+    // cout << "Введите сумму депозита в евро: ";
+    // cin >> deposit;
+
+    // cout << "Введите курс доллара к евро для 12 месяцев:\n";
+    // for (int i = 0; i < 12; i++) {
+    //     cout << "Месяц " << i + 1 << ": ";
+    //     cin >> rate[i];
+    // }
+
+    // cout << "Введите процент начисления по депозиту для 12 месяцев:\n";
+    // for (int i = 0; i < 12; i++) {
+    //     cout << "Месяц " << i + 1 << ": ";
+    //     cin >> percent[i];
+    // }
+
+    // cout << "Введите номер месяца (1-12): ";
+    // cin >> month;
+
+
+    // double accrued = deposit * (percent[month - 1] / 100);
+    // double dollars = accrued * rate[month - 1];
+
+    // if (dollars >= 500) {
+    //     double available = dollars * 0.5;
+    //     cout << "Можно обналичить: " << available << " $";
+    // } else {
+    //     cout << "Сумма меньше 500$, обналичивание невозможно";
+    // }
+
+
+
+
+}
---
+++ Nested_loops/1.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+
+    // №1
+
+    // for(int i = 0; i < 5; i++){
+    //         for(int j = 0; j < 7; j++){
+    //             if(i == 0 || i == 4 || j == 0 || j == 6){
+    //                 cout<< "*";
+    //             }
+    //             else{
+    //                 cout<< " ";
+    //             }
+    //         }
+    //     cout<< endl;
+    // }
+
+
+    // №2
+
+    // int n;
+    // cout<< "Введите ширину: ";
+    // cin>> n;
+
+    // int m;
+    // cout<< "Введите высоту: ";
+    // cin>> m;
+
+    // int b;
+    // b = n + m;
+
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < m; j++){
+    //         if(m % 2 == 0){
+    //             cout<< 1;
+    //         }
+    //         else{
+    //             cout<< 0;
+    //         }
+    //     }
+    //     cout<< endl;
+    // }
+
+
+    // №3
+
+    // int n;
+
+    // for(int i = 1; i < 4; i++){
+    //     for(int j = 1; j < 4; j++){
+    //         n = i + j;
+    //         cout<< n;
+    //     }
+    //     cout<< endl;
+    // }
+
+
+    // №4
+
+    // for(int i = 0; i < 4; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         if(i == j){
+    //             cout<< "*";
+    //         }
+    //         else if(i > j){
+    //             cout<< "+";
+    //         }
+    //         else{
+    //             cout<< "-";
+    //         }
+    //     }
+    //     cout<< endl;
+    // }
+
+
+    // №5
+
+    // for(int i = 0; i < 5; i++){
+    //     for(int j = 5; j > 0; j--){
+    //         if(j > i){
+    //             cout<< " ";
+    //         }
+    //         else{
+    //             cout<< " *";
+    //         }
+    //     }
+    //     cout<< endl;
+    // }
+    // for(int i = 3; i > 0; i--){
+    //     for(int j = 5; j > 0; j--){
+    //         if(j > i){
+    //             cout<< " ";
+    //         }
+    //         else{
+    //             cout<< " *";
+    //         }
+    //     }
+    //     cout<< endl;
+    // }
+
+
+    // №6
+
+    // for(int i = 1; i <= 10; i++){
+    //     for(int j = 1; j <= 10; j++){
+    //         int n = i * j;
+    //         cout<< i << " * " << j << " = " << n << "   ";
+    //         if(n < 10){
+    //             cout<< " ";
+    //         }
+    //         if(i < 10){
+    //             cout<< " ";
+    //         }
+    //         if(j < 10){
+    //             cout<< " ";
+    //         }
+    //     }
+    //     cout<< endl;
+    // }
+}
