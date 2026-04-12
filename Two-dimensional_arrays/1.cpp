5fe2415c5ab47bb6868f77eba55b936e4305c48d
Reinvu <shapavel2000@gmail.com>
2026-04-12 14:22:16 +0500

    Двумерные массивы

---
+++ Two-dimensional_arrays/1.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+int main(){
+
+
+    // №1 
+    
+    // int arr[4][4];
+    // srand(time(0));
+
+    // for(int i = 0; i < 4; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         arr[i][j] = rand() % 20 + 1;
+    //     }
+    // }
+    // for(int i = 0 ; i < 4; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         cout<< arr[i][j] << " ";
+    //     }
+    //     cout<< endl;
+    // }
+
+    
+    // №2
+
+    // int arr[4][4];
+    // srand(time(0));
+    // int sum = 0;
+
+    // for(int i = 0; i < 4; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         arr[i][j] = rand() % 100 + 1;
+    //         sum += arr[i][j];
+    //     }
+    // }
+    // for(int i = 0; i < 4; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         cout<< arr[i][j] << " ";
+    //     }
+    //     cout<< endl;
+    // }
+    // cout << "Сумма всех элементов мвссива: " << sum;
+
+
+    // №3
+
+    // int arr[4][4];
+    // srand(time(0));
+
+
+    // for(int i = 0; i < 4; i ++){
+    //     for(int j = 0; j < 4; j++){
+    //         arr[i][j] = rand() % 100 + 1;
+    //     }
+    // }
+    
+    // int max = arr[0][0];
+    // int min = arr[0][0];
+
+    // for(int i = 0; i < 4; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         if(max < arr[i][j]){
+    //             max = arr[i][j];
+    //         }
+    //         if(min > arr[i][j]){
+    //             min = arr[i][j];
+    //         }
+    //         cout<< arr[i][j] << " ";
+    //     }
+    //     cout<< endl;
+    // }
+    // cout<< "Максимальное число в массиве: " << max << endl;
+    // cout<< "Минимальное число в массиве: " << min; 
+
+   
+    // №4
+
+    // int arr[4][4];
+    // srand(time(0));
+    // int sum = 0;
+
+    // for(int i = 0; i < 4; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         arr[i][j] = rand() % 100 + 1;
+    //         sum += arr[i][j];
+    //     }
+    //     cout<< "Сумма " << i+1 << " строки равна = " << sum << endl;
+    //     sum = 0;
+    // }
+    // for(int i = 0; i < 4; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         cout<< arr[i][j] << " ";
+    //     }
+    //     cout<< endl;
+    // }
+
+
+    // №5
+
+    // int arr[4][4];
+    // srand(time(0));
+    // int chet = 0;
+
+    // for(int i = 0; i < 4; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         arr[i][j] = rand() % 100 + 1;
+    //         if(arr[i][j] % 2 == 0){
+    //             chet += 1;
+    //         }
+    //     }
+    // }
+    // for(int i = 0 ; i < 4; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         cout<< arr[i][j] << " ";
+    //     }
+    //     cout<< endl;
+    // }
+    // cout<< "Количество чётных элементов в массиве = " << chet;
+
+
+    // №6
+
+    // int arr[5][5];
+    // srand(time(0));
+    // int a = 0;
+
+    // for(int i = 0; i < 5; i++){
+    //     for(int j = 0; j < 5; j++){
+    //         arr[i][j] = rand() % 9 + 1;
+    //     }
+    // }
+    // for(int i = 0 ; i < 5; i++){
+    //     for(int j = 0; j < 5; j++){
+    //         if(arr[i][j] == arr[0+a][0+a]){
+    //             cout<< arr[i][j] << " ";
+    //             a += 1;
+    //         }
+    //         else{
+    //             cout<< "  ";
+    //         }
+            
+    //     }
+    //     cout<< endl;
+    // }
+
+    // cout<< endl;
+
+    // for(int i = 0 ; i < 5; i++){
+    //     for(int j = 0; j < 5; j++){
+    //         cout<< arr[i][j] << " ";
+    //     }
+    //     cout<< endl;
+    // }
+    
+}
