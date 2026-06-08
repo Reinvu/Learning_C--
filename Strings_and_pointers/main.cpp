df247162888bbe9f22e34a2e0b108981be788b05
Reinvu <shapavel2000@gmail.com>
2026-06-08 23:15:58 +0500

    Строки и указатели

---
+++ Strings_and_pointers/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    
+    // №1
+    
+    // cout<<"Enter str: " << endl;
+    // string str;
+    // getline(cin, str);
+    // cout<< "Lengh str: " << str.length() << endl;
+
+
+    // №2
+
+    // cout<< "Enter str: ";
+    // string str;
+    // cin>> str;
+    // for(int i = 0; i < str.length(); i++){
+    // cout<< str[i] << endl;
+    // }
+
+
+    // №3
+
+    // string str;
+    // cout<< "Enter str: ";
+    // getline(cin, str);
+    
+    // for(int i = 0; i < str.length() -1; i++){
+    //     if(i == 0){
+    //         cout<< "*";
+    //     }
+    //     else{
+    //         cout<< str[i];
+    //     }
+    // }
+    // cout<< "*";
+
+
+    // №4
+
+    // string str;
+    // cout<< "Enter str: ";
+    // getline(cin, str);
+    // if(str.find("hello") != string::npos){
+    //     cout<< "Yes";
+    // }
+    // else{
+    //     cout<< "No";
+    // }
+
+
+
+
+    //№1 and №2
+
+    // int x = 5;
+    // int *p = &x;
+    // cout<< *p << endl;
+    // cout<< p << endl;
+
+    // *p = 20;
+
+    // cout<< x << endl;
+    // cout<< *p << endl;
+
+
+    //№3
+
+    // int arr[5] = {10, 20, 30, 40, 50};
+    // int *p = arr;
+    // for(int i = 0; i < 5; i++){
+    //     cout<< *(p + i) << endl;
+    // }
+
+
+    //№4
+
+    // int x;
+    // cout<< "Enter number: ";
+    // cin>> x;
+    // int *p = &x;
+
+    // *p += 5;
+    // cout<< *p;
+
+}
